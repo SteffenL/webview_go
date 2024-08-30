e32f4e9c5e6f918969d7571986591fc13be920be
Steffen André Langnes <steffenl.sw@gmail.com>
2024-08-31 02:53:24 +0900

    Use build constraints to select WebKitGTK API

---
+++ backend/backend_win32_edge.go
@@ -0,0 +1,13 @@
+// +build windows
+// +build webview.backend.win32_edge
+
+package backend
+
+/*
+#cgo CFLAGS: -I${SRCDIR}/../libs/webview/include
+#cgo CXXFLAGS: -I${SRCDIR}/../libs/webview/include -I${SRCDIR}../libs/mswebview2/include -DWEBVIEW_STATIC -DWEBVIEW_EDGE -std=c++14
+#cgo LDFLAGS: -static -ladvapi32 -lole32 -lshell32 -lshlwapi -luser32 -lversion
+
+#include "webview.h"
+*/
+import "C"
