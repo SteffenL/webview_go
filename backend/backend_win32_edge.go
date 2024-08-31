32131dd79ece8fbd74c20d75346244b03c4f3ac5
Steffen André Langnes <steffenl.sw@gmail.com>
2024-08-31 19:38:27 +0900

    Fix include dir on Windows

--- backend/backend_win32_edge.go
+++ backend/backend_win32_edge.go
@@ -5,7 +5,7 @@ package backend
 
 /*
 #cgo CFLAGS: -I${SRCDIR}/../libs/webview/include
-#cgo CXXFLAGS: -I${SRCDIR}/../libs/webview/include -I${SRCDIR}../libs/mswebview2/include -DWEBVIEW_STATIC -DWEBVIEW_EDGE -std=c++14
+#cgo CXXFLAGS: -I${SRCDIR}/../libs/webview/include -I${SRCDIR}/../libs/mswebview2/include -DWEBVIEW_STATIC -DWEBVIEW_EDGE -std=c++14
 #cgo LDFLAGS: -static -ladvapi32 -lole32 -lshell32 -lshlwapi -luser32 -lversion
 
 #include "webview.h"
